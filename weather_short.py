72f1a3022c1d3e92ece17f1d7a8916c11b804c0c
ttadahira <tada.0112.tt@gmail.com>
2022-06-13 15:38:08 +0900

    いろいろ

--- weather_short.py
+++ weather_short.py
@@ -21,6 +21,9 @@ with open('data/new_weather_data.csv', 'r', encoding='utf-8') as f:
 
 #dateの初期値はFalse
 chosen_date = False
+chosen_year = False
+chosen_month = False
+chosen_day = False
 
 app = Flask(__name__)
 
@@ -28,16 +31,22 @@ app = Flask(__name__)
 @app.route('/')
 def top():
     #トップページに遷移するとdateを初期化する
-    global chosen_date
+    global chosen_date, chosen_year, chosen_month, chosen_day
     chosen_date = False
+    chosen_year = False
+    chosen_month = False
+    chosen_day = False
     return render_template('top.html')
 
 # トップページ(ロゴからの遷移用)
 @app.route('/', methods = ["POST"])
 def return_top():
     #トップページに遷移するとdateを初期化する
-    global chosen_date
+    global chosen_date, chosen_year, chosen_month, chosen_day
     chosen_date = False
+    chosen_year = False
+    chosen_month = False
+    chosen_day = False
     return render_template('top.html')
 
 # 日付選択ページ
@@ -64,27 +73,30 @@ def select_date():
                 if date_time > latest_list[0]:
                     latest_list = [date_time, get_date]
     # 古い日付をグローバル化
-    global oldest_year, oldest_month, oldest_date
+    global oldest_year, oldest_month, oldest_day
     oldest_year = oldest_list[1][0]
     oldest_month = oldest_list[1][1]
-    oldest_date = oldest_list[1][2]
+    oldest_day= oldest_list[1][2]
     # 新しい日付をグローバル化
-    global latest_year, latest_month, latest_date
+    global latest_year, latest_month, latest_day
     latest_year = latest_list[1][0]
     latest_month = latest_list[1][1]
-    latest_date = latest_list[1][2]
+    latest_day = latest_list[1][2]
     # htmlに渡す日付データの作成
     date = ",".join(date_list)
     # select_date.htmlを実行
-    return render_template('select_date.html', date=date, oldest_year=oldest_year, oldest_month=oldest_month, oldest_date=oldest_date ,latest_year=latest_year, latest_month=latest_month, latest_date=latest_date)
+    return render_template('select_date.html', date=date, oldest_year=oldest_year, oldest_month=oldest_month, oldest_day=oldest_day ,latest_year=latest_year, latest_month=latest_month, latest_day=latest_day)
 
 @app.route('/index', methods = ["POST"])
 def index():
     # ユーザに選択された日付
     date = request.form['date']
-    global chosen_date
-    chosen_date = date
-    return render_template('select_region.html', chosen_date=chosen_date)
+    global chosen_date, chosen_year, chosen_month, chosen_day
+    chosen_date = date.split('/')
+    chosen_year = chosen_date[0]
+    chosen_month = chosen_date[1]
+    chosen_day = chosen_date[2]
+    return render_template('select_region.html', chosen_year=chosen_year, chosen_month=chosen_month, chosen_day=chosen_day)
 
 @app.route('/select_region', methods = ["POST", "GET"])
 def select_region():
@@ -93,11 +105,11 @@ def select_region():
 @app.route('/select_region/<area>', methods = ["POST", "GET"])
 def select_area(area):
     area = str(area)
-    return render_template(f'area/{area}.html', chosen_date=chosen_date)
+    return render_template(f'area/{area}.html', chosen_year=chosen_year, chosen_month=chosen_month, chosen_day=chosen_day)
 
 # カレンダー表示ページ
-@app.route('/pick_city/<city>')
-def pick_city(city):
+@app.route('/pick_city/<region>/<city>', methods = ["POST", "GET"])
+def pick_city(region, city):
     count = 0
     data_str = ""
     for line in data:
@@ -108,7 +120,7 @@ def pick_city(city):
                 data_str = data_str + line_str
             else:
                 data_str = data_str + '\n' + line_str
-    return render_template('calendar.html', city=city, chosen_date=chosen_date, data=data_str)
+    return render_template('calendar.html', region=region, city=city, chosen_date=chosen_date, chosen_year=chosen_year, chosen_month=chosen_month, chosen_day=chosen_day, data=data_str)
 
 
 # 実行
